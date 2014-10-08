e749bc8ab9a18b3e019ae8f28a064d8937358a73
Pat Pannuto <pat.pannuto@gmail.com>
2014-10-08 15:53:57 -0400

    re-tab setup.py

--- setup.py
+++ setup.py
@@ -4,38 +4,38 @@ import sys
 from setuptools import setup, find_packages
 
 def readme():
-    with open('README.rst') as f:
-        return f.read()
+	with open('README.rst') as f:
+		return f.read()
 
 from abbreviate import __version__
 
 requires = [
-    'pyenchant',
-    ]
+		'pyenchant',
+		]
 
 setup(name='abbreviate',
-    version=__version__,
-    description="Automatically abbreviate text",
-    long_description=readme(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Topic :: Text Processing :: Filters",
-    ],
-    keywords='string formatting',
-    author="Pat Pannuto",
-    author_email="pat.pannuto+abbreviate@gmail.com",
-    url="https://github.com/ppannuto/python-abbreviate",
-    license="MIT",
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=requires,
-    #entry_points = {
-    #    'console_scripts':['abbreviate = abbreviate:console']
-    #},
-)
+		version=__version__,
+		description="Automatically abbreviate text",
+		long_description=readme(),
+		classifiers=[
+			"Development Status :: 3 - Alpha",
+			"Intended Audience :: Developers",
+			"Programming Language :: Python",
+			"License :: OSI Approved :: MIT License",
+			"Natural Language :: English",
+			"Topic :: Text Processing :: Filters",
+			],
+		keywords='string formatting',
+		author="Pat Pannuto",
+		author_email="pat.pannuto+abbreviate@gmail.com",
+		url="https://github.com/ppannuto/python-abbreviate",
+		license="MIT",
+		packages=find_packages(),
+		include_package_data=True,
+		zip_safe=False,
+		install_requires=requires,
+		#entry_points = {
+		#    'console_scripts':['abbreviate = abbreviate:console']
+		#},
+		)
 
